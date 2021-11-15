fb2518879f7949955533028fb3039772e4727d63
TADAS DELININKAITIS <t4dam94@gmail.com>
2021-11-15 20:37:26 +0200

    8:korteliu atvaizdavimas

--- js/components/ApartmentCardComponent.js
+++ js/components/ApartmentCardComponent.js
@@ -4,11 +4,46 @@ class ApartmentCardComponent {
     this.init();
   }
   init = () => {
+    const {
+      type,
+      owner: { fullname, email, phone },
+      roomCount,
+      squares,
+      address: { city, country, street, number },
+      price: { amount, currency },
+      imgSrc,
+    } = this.props;
+
+    const USD_EUR = 0.87;
+
+    const finalPrice = currency === "$" ? amount * USD_EUR : amount;
+    const formatedPrice = Math.round(100 * finalPrice) / 100 + " €";
+
     this.htmlElement = document.createElement("article");
     this.htmlElement.className = "card shadow";
     this.htmlElement.innerHTML = `
-    
-    <h2 class="h5">Kortelė</h2>
+  <img src="${imgSrc}" class="card-img-top"">
+  <div class="card-body">
+  <h2 class="h6">${type} for sale</h2>
+  <span>${street} - ${number}, ${city}, ${country}</span>
+
+
+  <div><strong>${roomCount} rooms | ${squares} m<sup>2</sup></strong></div>
+  
+     <div><strong>${formatedPrice}</strong></div>
+ 
+ <ul style="list-style-type:none">
+ <li>
+ Seller: ${fullname}
+ </li>
+ <li>
+ Phone: ${phone}
+ </li>
+ <li>
+ Phone: ${email}
+ </li>
+ 
+ </ul>
     `;
   };
 }
